57ee945c9d6f6b6b29b5e924cb6cbbbb712adc5d
Justin Howard <justin@epic-software.co.uk>
2017-02-12 14:45:20 +0000

    Made nav and logo respinsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
-        <div class="container pre-nav">
-            <a href="/"><img src="/logo.png" /></a>
-            <ul class="account-nav">
-                <li><a href="/account">My Account</a></li>
-                <li><a href="/cart"><i class="material-icons">shopping_cart</i> Cart</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m12 l8">
+                    <a class="logo" href="/"><img src="/logo.png" /></a>
+                </div>
+                <div class="col s12 m12 l4">
+                    <ul class="account-nav">
+                        <li v-if="isLoggedIn"><a href="/account">My Account</a></li>
+                        <li v-if="!isLoggedIn"><a href="/login">Login/Register</a></li>
+                        <li><a href="/cart"><i class="material-icons">shopping_cart</i> Cart</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
         <nav>
             <div class="container">
@@ -22,65 +29,72 @@
     </div>
 </template>
 
+<script>
+    const currentUser = {};
+
+    export default {
+        props: {
+            currentUser: currentUser
+        },
+        beforeMount: function (){
+            if (this.currentUser){
+                this.isLoggedIn = this.currentUser.token ? true: false;
+            }
+        },
+        data: function(){
+            return {
+                isLoggedIn: false,
+            }
+        }
+    }
+
+</script>
+
 <style>
     nav {
         background-color: #AB82FF !important;
     }
 
-    .pre-nav {
-        width: 100%;
-        background-color: #ffffff !important;
-        position: relative;
-        height: 100px;
-    }
-
-    .pre-nav a img {
+    .logo {
         height: 50px;
-        top: 25px;
-        position: absolute;
     }
 
-    .pre-nav .account-nav {
-        position: absolute;
-        width: 30%;
+    .account-nav {
         list-style: none;
-        bottom: -15px;
-        right: 0;
         height: 50px;
     }
 
-    .pre-nav .account-nav li {
+    .account-nav li {
         width: 33.3% !important;
         border-right: 1px solid #AB82FF;
-        float: left;
         height: 70%;
         justify-content: center;
         text-align: center;
         align-items: center;
         display: flex;
+        float: left;
     }
 
-    .pre-nav .account-nav li:last-child {
+    .account-nav li:last-child {
         border-right: none;
     }
 
-    .pre-nav .account-nav li a {
+    .account-nav li a {
         color: #AB82FF;
         vertical-align: middle;
     }
 
-    .pre-nav .account-nav li i {
+    .account-nav li i {
         position: relative;
         vertical-align: bottom;
     }
 
-    .pre-nav .account-nav li a:hover {
+    .account-nav li a:hover {
         color: #8968CD;
     }
 
     .main-nav-ul li {
         text-transform: uppercase;
     }
-
 </style>
 
