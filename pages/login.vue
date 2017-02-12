6a8df36c6d3b7c18a8dcfa5f66089cc31a2f80f3
Justin Howard <justin@epic-software.co.uk>
2017-02-12 14:59:31 +0000

    Started on login/register page

---
+++ pages/login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <nav class="nav-breadcrumb">
+                <div class="nav-wrapper">
+                    <div class="col s12">
+                        <a href="/" class="breadcrumb">Home</a>
+                        <a href="#!" class="breadcrumb">Login/Register</a>
+                    </div>
+                </div>
+            </nav>
+            <div class="col s12">
+                <h4 class="intro">Login or Create an account</h4>
+            </div>
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+    .nav-breadcrumb {
+        margin-top: 30px !important;
+        background-color: #9E9E9E !important;
+    }
+
+    .intro {
+        text-align: center;
+        margin-top: 50px;
+        color: #AB82FF;
+    }
+</style>
+<script>
+
+</script>
